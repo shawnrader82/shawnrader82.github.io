8546dba92423bab73bf54c9ada8acc731c2f0742
shawnrader82 <shawn@mobileamericannotary.com>
2026-03-09 11:58:55 -0700

    Overwrite site with clean desktop copy

---
+++ order-form/nc-config.php
@@ -0,0 +1,6 @@
+<?php
+// nc-config.php - DO NOT COMMIT TO GIT
+$ncBaseUrl = 'https://cloud.mobileamericannotary.com/remote.php/dav/files/shawn/';
+$ncUser    = 'shawn@mobileamericannotary.com';
+$ncPass    = 'BH7YH-MGcbo-Jd8ZW-BaAEY-fTcrz';
+
